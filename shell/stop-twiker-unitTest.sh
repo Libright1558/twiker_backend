0f597b81d908c0c74aa12a0fff2c126fca09bf0f
libright1558 <n32717325@gmail.com>
2024-11-24 23:04:13 +0800

    Stop the runtime of unit tests

--- shell/stop-twiker-unitTest.sh
+++ shell/stop-twiker-unitTest.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker exec -w /source twiker-app-dev-container dotnet test
+docker compose  -f ./docker-compose-unittest.yml -p twiker-docker down --remove-orphans 
+
+docker rmi twiker-app-unittest
